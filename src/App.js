f4376ddd3bac6fdfe7c851bb9ab6f8512a12ad1f
lihel <835943102@qq.com>
2020-11-18 20:24:19 +0800

    feat: 井字棋

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import ShoppingList from "./components/TicTacToe.js";
+import Game from "./components/TicTacToe.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">344</header>
+      <header className="App-header">header</header>
       <Router>
-        <Route path="/" component={ShoppingList} />
+        <Route path="/" component={Game} />
       </Router>
     </div>
   );
