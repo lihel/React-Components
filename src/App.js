680158794dd9e2b357a8de75e2c6278f49b10b04
lihel <835943102@qq.com>
2020-11-18 16:53:23 +0800

    feat: 引入组件

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import ShoppingList from "./components/TicTacToe.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        23333
-      </header>
+      <header className="App-header">23333</header>
+      <Router>
+        <Route path="/" component={ShoppingList} />
+      </Router>
     </div>
   );
 }
---
+++ src/components/TicTacToe.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default class ShoppingList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Shopping List</h1>
+        <ul>
+          <li>Instagram</li>
+          <li>WhatsApp</li>
+          <li>Oculus</li>
+        </ul>
+      </div>
+    );
+  }
+}
