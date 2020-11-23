4f757b11eea2468818ca324db5fae9ae838df525
lihel <835943102@qq.com>
2020-11-23 17:31:00 +0800

    config

--- webpack.config.js
+++ webpack.config.js
@@ -7,13 +7,13 @@ module.exports = {
   },
 
   devServer: {
-    publicPath: "/",
+    // publicPath: "/",
     contentBase: "./build", // 服务启动在哪一个文件夹下
-    open: true, // 启动服务时，自动打开浏览器
-    port: 8080, // 端口号
-    // proxy 跨域时模拟接口代理
-    hot: true, // devServer开启Hot Module Replacement的功能
-    hotOnly: true, // 即便HMP的功能没有生效，浏览器也不能自动刷新
+    // open: true, // 启动服务时，自动打开浏览器
+    // port: 8080, // 端口号
+    // // proxy 跨域时模拟接口代理
+    // hot: true, // devServer开启Hot Module Replacement的功能
+    // hotOnly: true, // 即便HMP的功能没有生效，浏览器也不能自动刷新
   },
   module: {
     rules: [
